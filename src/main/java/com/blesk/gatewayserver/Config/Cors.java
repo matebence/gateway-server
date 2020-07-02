8a4be0988f0cb8267bf1945bfbf29ff938f7e27f
Bence <m.bence05@gmail.com>
2020-07-02 14:46:15 +0200

    "Access-Control-Allow-Origin", "*"

--- src/main/java/com/blesk/gatewayserver/Config/Cors.java
+++ src/main/java/com/blesk/gatewayserver/Config/Cors.java
@@ -12,11 +12,9 @@ public class Cors implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4300/");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Max-Age", "180");
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
