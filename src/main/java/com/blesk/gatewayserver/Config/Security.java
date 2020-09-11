9a7c8b6b0f4e5370494601aa74d1c3fb37550402
Bence <m.bence05@gmail.com>
2020-09-11 20:53:38 +0200

    corsFilter

--- src/main/java/com/blesk/gatewayserver/Config/Security.java
+++ src/main/java/com/blesk/gatewayserver/Config/Security.java
@@ -1,6 +1,5 @@
 package com.blesk.gatewayserver.Config;
 
-import com.netflix.ribbon.proxy.annotation.Http;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -13,12 +12,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.stream.Collectors;
 
 @Configuration
 public class Security extends WebSecurityConfigurerAdapter {
@@ -44,34 +39,22 @@ public class Security extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public CorsFilter corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("*"));
-        config.setAllowedHeaders(Collections.singletonList("*"));
-        config.setAllowedMethods(Arrays.stream(Http.HttpMethod.values()).map(Http.HttpMethod::name).collect(Collectors.toList()));
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
-//    @Bean
-//    public FilterRegistrationBean corsFilter() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setMaxAge(this.age);
-//        config.setAllowCredentials(true);
-//        config.setAllowedOrigins(Arrays.asList(this.origins.split(", ")));
-//        config.setAllowedHeaders(Arrays.asList(this.headers.split(", ")));
-//        config.setAllowedMethods(Arrays.asList(this.methods.split(", ")));
-//        config.setExposedHeaders(Arrays.asList(this.exposed.split(", ")));
-//
-//        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new CorsFilter(source));
-//        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-//
-//        return filterRegistrationBean;
-//    }
+        CorsConfiguration config = new CorsConfiguration();
+        config.setMaxAge(this.age);
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList(this.origins.split(", ")));
+        config.setAllowedHeaders(Arrays.asList(this.headers.split(", ")));
+        config.setAllowedMethods(Arrays.asList(this.methods.split(", ")));
+        config.setExposedHeaders(Arrays.asList(this.exposed.split(", ")));
+
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new CorsFilter(source));
+        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        return filterRegistrationBean;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
